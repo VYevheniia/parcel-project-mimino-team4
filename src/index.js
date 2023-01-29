59fa77271a1ecee8d54568c21ac5694065f1b1c1
VYevheniia <117318337+VYevheniia@users.noreply.github.com>
2023-01-29 16:10:39 +0100

    додала плагіни, підключила фавіконку

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-
+import './js/slider-con';
+import './js/select-set';
+import './js/modals-set';
+import './js/modal-price';
+import './js/modal-room';
+import './js/modal-table';
+import './js/imask-set';
+import './js/vendors/nice-select2';
